54a1aa971e397db4f2ff0848ff8feacf2a4d1e88
Andreas Pardeike <andreas@pardeike.net>
2022-09-22 22:18:24 +0200

    initial checkin

---
+++ main.js
@@ -0,0 +1,95 @@
+const { app, BrowserWindow, session, powerSaveBlocker } = require('electron')
+const path = require('path')
+
+// app.commandLine.appendSwitch('disable-gpu')
+// app.commandLine.appendSwitch('disable-gpu-compositing')
+// app.commandLine.appendSwitch('disable-accelerated-video-decode')
+// app.commandLine.appendSwitch('disable-accelerated-video-encode')
+// powerSaveBlocker.start('prevent-app-suspension')
+
+const createWindow = () => {
+
+  session.defaultSession.webRequest.onBeforeRequest({
+    urls: ['https://embed.twitch.tv/*channel=*']
+  }, (details, cb) => {
+    var redirectURL = details.url
+
+    var params = new URLSearchParams(redirectURL.replace('https://embed.twitch.tv/', ''))
+    if (params.get('parent') != '') {
+      cb({})
+      return
+    }
+    params.set('parent', 'locahost')
+    params.set('referrer', 'https://localhost/')
+
+    var redirectURL = 'https://embed.twitch.tv/?' + params.toString()
+    console.log('Adjust to', redirectURL)
+
+    cb({
+      cancel: false,
+      redirectURL
+    })
+  })
+
+  session.defaultSession.webRequest.onHeadersReceived({
+    urls: [
+      'https://www.twitch.tv/*',
+      'https://player.twitch.tv/*',
+      'https://embed.twitch.tv/*'
+    ]
+  }, (details, cb) => {
+    var responseHeaders = details.responseHeaders
+
+    console.log('headers', details.url, responseHeaders)
+
+    delete responseHeaders['Content-Security-Policy']
+
+    cb({
+      cancel: false,
+      responseHeaders
+    })
+  })
+
+  const win = new BrowserWindow({
+    title: 'StreamViewer',
+    backgroundThrottling: false,
+    frame: false,
+    titleBarStyle: 'hidden',
+    resizable: false,
+    transparent: false,
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      disablewebsecurity: true,
+      preload: path.join(__dirname, 'preload.js'),
+      //partition: 'persist:rimionship'
+    }
+  })
+
+  win.webContents.on('before-input-event', (event, input) => {
+    if (input.control) {
+      const key = input.key.toLowerCase()
+      if (key === 'f') win.loadURL('https://frontend.rimionship.com')
+      if (key === 't') win.loadURL('https://twitch.tv')
+      if (key === 'r') win.loadFile('index.html')
+      if (key === 'c') win.loadFile('empty.html')
+      if (key === 'd') win.webContents.openDevTools()
+      event.preventDefault()
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0)
+      createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ renderer.js
@@ -0,0 +1,61 @@
+var options = {
+  width: 1280,
+  height: 720,
+  channel: "robbaz",
+  transparent: true,
+  allowfullscreen: true,
+  layout: "video",
+  muted: false
+}
+var player = new Twitch.Embed("twitch", options)
+
+// player.addEventListener(Twitch.Embed.VIDEO_READY, function () {
+//   console.log('The video is ready')
+// })
+//
+// player.addEventListener(Twitch.Embed.VIDEO_PLAY, function () {
+//   console.log('The video is playing')
+// })
+
+var currentStreamer = ''
+
+function handleErrors(response) {
+  if (!response.ok) {
+    window.location.reload()
+    return
+  }
+  return response.json()
+}
+
+async function setStreamerId() {
+  try {
+    await fetch(`https://frontend.rimionship.com/api/showstream/0/0?handler=Streamer`, {
+      method: "get"
+    })
+      .then(response => handleErrors(response))
+      .then(async (data) => {
+        if (currentStreamer !== data.streamer) {
+          currentStreamer = data.streamer
+          player.setChannel(currentStreamer)
+          player.setQuality('chunked')
+          document.getElementById('streamer').innerHTML = currentStreamer
+        }
+      })
+  } catch (e) {
+    console.log(e.message)
+    debugger
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function run() {
+  while (true) {
+    await sleep(1000)
+    await setStreamerId()
+  }
+}
+
+run().then(_ => { })
